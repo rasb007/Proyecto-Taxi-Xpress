a3c781da35435b8c54bfbac4e3237630020b7e43
ateus <christian_elche300@outlook.com>
2015-11-09 01:57:25 +0000

    actualizacion

---
+++ app-taxixpress/app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def nosotros
+  end
+
+  def contacto
+  end
+
+  def reserva
+  end
+
+  def registro
+  end
+
+  def servicios
+  end
+end
---
+++ app-taxixpress/app/views/profiles/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @profile, :id, :nombre, :apellido, :nombre_empresa, :ruc, :direccion, :email, :contraseña, :user_id, :created_at, :updated_at
